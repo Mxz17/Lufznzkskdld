e5c9f29542ee4e96c0e6eef850799e65a7102c40
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-24 20:32:48 -0300

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
 let txt_owner = "> Hola, Este es el *numero* de mi *creadxr*, cualquier falla o si quieres agregar el bot a tu grupo, puedes *hablarle* ❀᭄ۣۜۜۜ\n\n ⋱ *+591 68683798* ⢫"
-await conn.sendFile(m.chat, "https://f.uguu.se/yOLZnbAX.jpg', txt_owner, m, null, rcanal)
+await conn.sendFile(m.chat, "https://f.uguu.se/yOLZnbAX.jpg"', txt_owner, m, null, rcanal)
 }
 handler.help = ['owner']
 handler.tags = ['main']
