63d86b952c945a8a88f9a089366a11f59380bc64
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-24 19:30:20 -0300

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let txt_owner = "> _*`Hola, Este es el numero de mi creadxr, cualquier falla o si quieres agregar el bot a tu grupo, puedes hablarle`*_\n\n `+54 9 381 645-7465`"
+let txt_owner = "> Hola, Este es el *numero* de mi *creadxr*, cualquier falla o si quieres agregar el bot a tu grupo, puedes *hablarle*\n\n *+591 68683798*"
 await conn.sendFile(m.chat, "https://th.bing.com/th/id/OIP.UrDEsrIiX93efNEOpMTezgHaEo?dpr=2&pid=ImgDetMain", 'thumbnail.jpg', txt_owner, m, null, rcanal)
 }
 handler.help = ['owner']
