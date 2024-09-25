4a610e3542677812e0ad9968f4ede94c8a588c9b
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-25 14:03:15 -0300

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -741,16 +741,16 @@ console.error(e)
 
 global.dfail = (type, m, conn) => {
 const msg = {
-rowner: '```🤍̸̷̸̷᮫᮫໋໋݂݂ׄׄ. ¡¡ESTA FUNCIÓN SOLO PUEDE SER USADA POR MI CREADOR!!```', 
-owner: '```🤍̸̷̸̷᮫᮫໋໋݂݂ׄׄ. ¡¡ESTA FUNCIÓN SOLO PUEDE SER USADA POR MI DESARROLLADOR!!```', 
-mods: '```🤍̸̷̸̷᮫᮫໋໋݂݂ׄׄ. ¡¡ESTA FUNCIÓN SOLO PUEDE SER USADA POR MIS DESARROLLADORES!!```', 
-premium: '```🤍̸̷̸̷᮫᮫໋໋݂݂ׄׄ. ¡¡ESTA FUNCIÓN SOLO ES PARA USUARIOS PREMIUM!!```', 
-group: '```🤍̸̷̸̷᮫᮫໋໋݂݂ׄׄ. ¡¡ESTA FUNCION SOLO PUEDE SER EJECUTADA EN GRUPOS!!```', 
-private: '```🤍̸̷̸̷᮫᮫໋໋݂݂ׄׄ. ¡¡ESTA FUNCIÓN SOLO PUEDE SER USADA EN CHAT PRIVADO!!```', 
-admin: '```🤍̸̷̸̷᮫᮫໋໋݂݂ׄׄ. ¡¡ESTE COMANDO SOLO PUEDE SER USADO POR ADMINS!!```', 
-botAdmin: '```🤍̸̷̸̷᮫᮫໋໋݂݂ׄׄ. ¡¡PARA USAR ESTA FUNCIÓN DEBO SER ADMIN DEL GRUPO!!```', 
-unreg: '```🤍̸̷̸̷᮫᮫໋໋݂݂ׄׄ. ¡¡NECESITAS ESTAR REGISTRADO(A) PARA USAR ESTE COMANDO, ESCRIBE #reg PARA REGISTRARTE!!```',
-restrict: '```🤍̸̷̸̷᮫᮫໋໋݂݂ׄׄ. ¡¡ESTA CARACTERÍSTICA ESTA DESACTIVADA!!```'
+rowner: '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ ¡¡ESTA *FUNCIÓN* SOLO *PUEDE* *SER* *USADA* POR *MI* *CREADOR!!*', 
+owner: '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ ¡¡ESTA *FUNCIÓN* SOLO *PUEDE* *SER* *USADA* POR *MI* *DESARROLLADOR!!*', 
+mods: '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ ¡¡ESTA *FUNCIÓN* SOLO *PUEDE* *SER* *USADA* POR *MIS* *DESARROLLADORES!!*', 
+premium: '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ ¡¡ESTA *FUNCIÓN* SOLO *ES* *PARA* *USUARIOS* *PREMIUM!!*', 
+group: '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ ¡¡ESTA *FUNCION* SOLO *PUEDE* SER *USADA* *EN* *GRUPOS!!*', 
+private: '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ ¡¡ESTA *FUNCIÓN* SOLO *PUEDE* SER *USADA* EN *CHAT PRIVADO!!*', 
+admin: '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ ¡¡ESTA *FUNCIÓN* SOLO *PUEDE* SER *USADA* POR *ADMINS!!*', 
+botAdmin: '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ ¡¡PARA USAR ESTA *FUNCIÓN* *DEBO* SER *ADMIN* DEL *GRUPO!!*', 
+unreg: '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ ¡¡NECESITAS ESTAR *REGISTRADO(A)* PARA *USAR* ESTE *COMANDO,* *ESCRIBE* *.reg PARA* *REGISTRARTE!!*',
+restrict: '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ ¡¡ESTA *CARACTERÍSTICA* ESTA *DESACTIVADA!!*'
 }[type];
 if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))}
 const file = global.__filename(import.meta.url, true);
