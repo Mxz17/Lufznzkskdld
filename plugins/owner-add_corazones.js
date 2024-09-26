fa21db5665e39b556aa80acafa85ad6190283982
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-26 01:49:49 -0300

    Actualizar owner-add_corazones.js

--- plugins/owner-add_corazones.js
+++ plugins/owner-add_corazones.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, text}) => {
   let who;
   if (m.isGroup) who = m.mentionedJid[0];
   else who = m.chat;
-  if (!who) throw '*etiqueta 🏷️ ala persona para agregarle sus corazones 🤍*';
+  if (!who) throw '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *etiqueta 🏷️ ala persona para agregarle sus corazones 🤍*';
   const txt = text.replace('@' + who.split`@`[0], '').trim();
   if (!txt) throw 'ingresa la cantidad de corazones 🤍 a agregar';
   if (isNaN(txt)) throw 'no se admiten símbolos solo números 🔢';
@@ -12,13 +12,13 @@ const handler = async (m, {conn, text}) => {
   let limit = dmt;
   const pjk = Math.ceil(dmt * pajak);
   limit += pjk;
-  if (limit < 1) throw '*el número mínimo de corazones a agregar es 1 🤍*';
+  if (limit < 1) throw '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *el número mínimo de corazones a agregar es 1 🤍*';
   const users = global.db.data.users;
   users[who].corazones += dmt;
-  m.reply(`*🤍 𝐀𝐆𝐑𝐄𝐆𝐀𝐃𝐎*
+  m.reply(`> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *AGREGADOS᭄*
 
-𝐓𝐨𝐭𝐚𝐥: ${dmt} 🤍
-𝐅𝐞𝐜𝐡𝐚: ${fecha} 📆`);
+⋆ͦ *𝐓𝐨𝐭𝐚𝐥 ::* ${dmt} 🤍
+⋆ͦ *𝐅𝐞𝐜𝐡𝐚 ::* ${fecha} 📆`);
 };
 handler.command = ['añadircorazones', 'addc', 'darc', 'darcorazones'];
 handler.rowner = true;
