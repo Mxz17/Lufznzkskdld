0ee99855d13594b44fad06962410cfb21cd420f6
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-26 01:59:14 -0300

    Actualizar mods-creargc.js

--- plugins/mods-creargc.js
+++ plugins/mods-creargc.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, text }) => {
-if (!text) return m.reply('🤍 Ingresa un nombre para el grupo.')
+if (!text) return m.reply('> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *Ingresa un nombre para el grupo.*')
 try{
-m.reply('🤍 *Creando grupo*')
+m.reply('> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *Creando grupo*')
 let group = await conn.groupCreate(text, [m.sender])
 let link = await conn.groupInviteCode(group.gid)
 m.reply('https://chat.whatsapp.com/' + url)
 } catch (e) {
-m.reply(`🤍 Ocurrió un error.`)
+m.reply(`> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *Ocurrió un error.*`)
 }
 }
 handler.help = ['grupocrear <nombre>']
