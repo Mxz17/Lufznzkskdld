b78507a74c1fe639c2df8cadb4e0e4379f4f0962
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-28 01:38:56 -0300

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -6,20 +6,20 @@ handler.all = async function(m) {
 
   if (/^bot$/i.test(m.text) && !chat.isBanned) {
     conn.sendPresenceUpdate('recording', m.chat);
-conn.reply(m.chat, `🤍 ¡Hola! Soy Ai Genesis, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
+conn.reply(m.chat, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ ¡Hola! Soy Luffy Ai, en que puedo ayudarte hoy?\n\n✡ Usa *.menu* para ver mis comandos.`, m, rcanal, )
     m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
 }
 if (/^sexo$/i.test(m.text)) {
-conn.reply(m.chat, `*pervertido* 🫣`, m, rcanal, )
+conn.reply(m.chat, `> *pervertido* 🫣᭄`, m, rcanal, )
 }
 if (/^tetas|teta$/i.test(m.text)) {
-conn.reply(m.chat, `*que caliente eres* 🥵`, m, rcanal, )
+conn.reply(m.chat, `> *que caliente eres* 🥵᭄`, m, rcanal, )
 }
 if (/^bug$/i.test(m.text)) {
-conn.reply(m.chat, `*tu mamá we* 😹`, m, rcanal, )
+conn.reply(m.chat, `> *tu mamá we* 😹᭄`, m, rcanal, )
 }
 if (/^pene$/i.test(m.text)) {
-conn.reply(m.chat, `*comes* 😹`, m, rcanal, )
+conn.reply(m.chat, `> *comes* 😹᭄`, m, rcanal, )
 }
   return !0;
 };
