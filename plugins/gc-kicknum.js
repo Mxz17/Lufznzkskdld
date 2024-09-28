e9e463175b7b51b91c3686119010ba724c7ad5db
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-28 02:53:48 -0300

    Actualizar gc-kicknum.js

--- plugins/gc-kicknum.js
+++ plugins/gc-kicknum.js
@@ -1,31 +1,31 @@
 
 const handler = async (m, {conn, args, groupMetadata, participants, usedPrefix, command, isBotAdmin, isSuperAdmin}) => {
-  if (!args[0]) return m.reply(`*📍Ingrese Algun Prefijo De Un Pais, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} 52*`);
-  if (isNaN(args[0])) return m.reply(`*📍Ingrese Algun Prefijo De Un Pais, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} 52*`);
+  if (!args[0]) return m.reply(`> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Ingrese Algun Prefijo De Un Pais, Ejemplo : ${usedPrefix + command} 51*`);
+  if (isNaN(args[0])) return m.reply(`> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Ingrese Algun Prefijo De Un Pais, Ejemplo : ${usedPrefix + command} 52*`);
   const lol = args[0].replace(/[+]/g, '');
   const ps = participants.map((u) => u.id).filter((v) => v !== conn.user.jid && v.startsWith(lol || lol));
   const bot = global.db.data.settings[conn.user.jid] || {};
-  if (ps == '') return m.reply(`*📍 Aqui No Hay Ningun Numero Con El Prefijo +${lol}*`);
-  const numeros = ps.map((v)=> '⭔ @' + v.replace(/@.+/, ''));
+  if (ps == '') return m.reply(`> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Aqui No Hay Ningun Numero Con El Prefijo +${lol}*`);
+  const numeros = ps.map((v)=> '⭟ @' + v.replace(/@.+/, ''));
   const delay = (time) => new Promise((res)=>setTimeout(res, time));
   switch (command) {
     case 'listanum': case 'listnum':
-      conn.reply(m.chat, `*𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙽𝚄𝙼𝙴𝚁𝙾𝚂 𝙲𝙾𝙽 𝙴𝙻 𝙿𝚁𝙴𝙵𝙸𝙹𝙾 +${lol} 𝚀𝚄𝙴 𝙴𝚂𝚃𝙰𝙽 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾:*\n\n` + numeros.join`\n`, m, {mentions: ps});
+      conn.reply(m.chat, `°·ꯥ̣ꩍ *Lista de los numeros con prefijo +${lol} 𝚀𝚄𝙴 están en este grupo :*\n\n` + numeros.join`\n`, m, {mentions: ps});
       break;
     case 'kicknum':
-      if (!bot.restrict) return m.reply('*¡Este Comando Esta Desabilitado Por El Propietario Del Bot!*');
-      if (!isBotAdmin) return m.reply('*¡📍 El Bot No Es Admin!*');
-      conn.reply(m.chat, `*⏰️ Espere Iniciando La Eliminación*`, m);
+      if (!bot.restrict) return m.reply('> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *¡Este Comando Esta Desabilitado Por El Propietario Del Bot!*');
+      if (!isBotAdmin) return m.reply('> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *¡El Bot No Es Admin!*');
+      conn.reply(m.chat, `> *⎇⸱ۣۜۜ⏰️᭄ Espere Iniciando La Eliminación*`, m);
       const ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net';
       const users = participants.map((u) => u.id).filter((v) => v !== conn.user.jid && v.startsWith(lol || lol));
       for (const user of users) {
-        const error = `@${user.split('@')[0]} ʏᴀ ʜᴀ sɪᴅᴏ ᴇʟɪᴍɪɴᴀᴅᴏ ᴏ ʜᴀ ᴀʙᴀɴᴅᴏɴᴀᴅᴏ ᴇʟ ɢʀᴜᴘᴏ*`;
+        const error = `@${user.split('@')[0]} Ya an sido un eliminado o ah abandonado el grupo▹*`;
         if (user !== ownerGroup + '@s.whatsapp.net' && user !== global.conn.user.jid && user !== global.owner + '@s.whatsapp.net' && user.startsWith(lol || lol) && user !== isSuperAdmin && isBotAdmin && bot.restrict) {
           await delay(2000);
           const responseb = await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
           if (responseb[0].status === '404') m.reply(error, m.chat, {mentions: conn.parseMention(error)});
           await delay(10000);
-        } else return m.reply('*✨️ 𝙴𝚁𝚁𝙾𝚁*');
+        } else return m.reply('> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Error*');
       }
       break;
   }
