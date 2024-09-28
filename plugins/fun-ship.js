882286bc537929e3e40d5d8a53e4e36554e25d60
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-28 12:57:38 -0300

    Actualizar fun-ship.js

--- plugins/fun-ship.js
+++ plugins/fun-ship.js
@@ -1,11 +1,11 @@
 var handler = async (m, { conn, command, text }) => {
 
-if (!text) return conn.reply(m.chat, '🤍  *Escribe el nombre de dos personas para calcular su amor*', m, rcanal, )
+if (!text) return conn.reply(m.chat, '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Escribe el nombre de dos personas para calcular su amor*', m, rcanal, )
 let [text1, ...text2] = text.split(' ')
 
 text2 = (text2 || []).join(' ')
 if (!text2) return conn.reply(m.chat, `🤍  *Escribe el nombre de la segunda persona*`, m, rcanal, )
-let love = `❤️ *${text1}* tu oportunidad de enamorarte de *${text2}* es de ${Math.floor(Math.random() * 100)}% 👩🏻‍❤️‍👨🏻`
+let love = `> ♡̩͙  *${text1}* tu oportunidad de enamorarte de *${text2}* es de ${Math.floor(Math.random() * 100)}% 👩🏻‍❤️‍👨🏻‎ꕀ`
 
 m.reply(love, null, { mentions: conn.parseMention(love) })
 
