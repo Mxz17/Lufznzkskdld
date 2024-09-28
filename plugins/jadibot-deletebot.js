738a0464eb84939c2fe2aef9092ef88773a86c03
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-28 01:05:33 -0300

    Actualizar jadibot-deletebot.js

--- plugins/jadibot-deletebot.js
+++ plugins/jadibot-deletebot.js
@@ -9,10 +9,10 @@ let userS = `${conn.getName(who)}`
 
 try {
 await fs.rmdir("./GenesisJadiBot/" + uniqid, { recursive: true, force: true })
-await parentw.sendMessage(m.chat, { text: '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *Sub-Bot eliminado.*' }, { quoted: fkontak })
+await parentw.sendMessage(m.chat, { text: '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Sub-Bot eliminado.*' }, { quoted: fkontak })
 } catch(err) {
 if (err.code === 'ENOENT' && err.path === `./GenesisJadiBot/${uniqid}`) {
-await parentw.sendMessage(m.chat, { text: "> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *No cuentas con ninguna sesión de Sub-Bot.*" }, { quoted: fkontak })
+await parentw.sendMessage(m.chat, { text: "> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *No cuentas con ninguna sesión de Sub-Bot.*" }, { quoted: fkontak })
 } else {
 await m.react(error)
 }}}
