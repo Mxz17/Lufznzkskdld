33861258eb598b105f6f33e6b677ef1dd669bb2b
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-28 01:03:33 -0300

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -12,12 +12,12 @@ global.conns = []
 }
 let handler = async (m, { conn, args, usedPrefix, command, isOwner, isPrems, isROwner }) => {
 if (!global.db.data.settings[conn.user.jid].jadibotmd && !isROwner) {
-conn.reply(m.chat, '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *Este Comando está deshabilitado por mi creador.*', m, rcanal)
+conn.reply(m.chat, '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Este Comando está deshabilitado por mi creador.*', m, rcanal)
 return
 }
 let parentw = args[0] && args[0] == "plz" ? conn : await global.conn
 if (!(args[0] && args[0] == 'plz' || (await global.conn).user.jid == conn.user.jid)) {
-return conn.reply(m.chat, `💭᭄ *Solo puedes usar este comando en el bot principal.*\n\n• Wa.me/${global.conn.user.jid.split`@`[0]}?text=${usedPrefix + command}`, m, rcanal)
+return conn.reply(m.chat, `> 💭᭄ *Solo puedes usar este comando en el bot principal.*\n\n• Wa.me/${global.conn.user.jid.split`@`[0]}?text=${usedPrefix + command}`, m, rcanal)
 }
 async function serbot() {
 let serbotFolder = crypto.randomBytes(10).toString('hex').slice(0, 8)
@@ -58,7 +58,7 @@ if (isNewLogin) {
 conn.isInit = true
 }
 if (qr) {
-let txt = '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ S E R B O T - S U B B O T᭄\n\n*Escanea este QR para ser un Sub Bot*\n\n🤍 Pasos para escanear:\n\n`1` : Haga click en los 3 puntos\n\n`2` : Toque dispositivos vinculados\n\n`3` : Escanea este QR\n\n> *Nota:* Este código QR expira en 30 segundos.'
+let txt = '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ S E R B O T - S U B B O T᭄\n\n*Escanea este QR para ser un Sub Bot*\n\n🍿 Pasos para escanear:\n\n`1` : Haga click en los 3 puntos\n\n`2` : Toque dispositivos vinculados\n\n`3` : Escanea este QR\n\n> *Nota:* Este código QR expira en 30 segundos.'
 
 let sendQR = await parentw.sendFile(m.chat, await qrcode.toDataURL(qr, { scale: 8 }), "qrcode.png", txt, m, null, rcanal)
 setTimeout(() => {
@@ -74,7 +74,7 @@ return console.log(await creloadHandler(true).catch(console.error))
 delete global.conns[i]
 global.conns.splice(i, 1)
 if (code !== DisconnectReason.connectionClosed) {
-await parentw.reply(conn.user.jid, "🤍 Conexión perdida con el servidor.", m)
+await parentw.reply(conn.user.jid, "🍿 Conexión perdida con el servidor.", m)
 }
 }
 if (global.db.data == null) {
@@ -83,12 +83,12 @@ loadDatabase()
 if (connection == "open") {
 conn.isInit = true
 global.conns.push(conn)
-await parentw.reply(m.chat, args[0] ? '🤍 Conectado con éxito al WhatsApp.' : '✅ *Conectado con éxito!*\n\nSi se desconecta se reconectara automáticamente, a menos que elimines la sesión\n\nEl número del bot puede cambiar, guarda este enlace: https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y', m, rcanal)
+await parentw.reply(m.chat, args[0] ? '🍿 Conectado con éxito al WhatsApp.' : '✅ *Conectado con éxito!*\n\nSi se desconecta se reconectara automáticamente, a menos que elimines la sesión\n\nEl número del bot puede cambiar, guarda este enlace: https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y', m, rcanal)
 await sleep(5000)
 if (args[0]) {
 return
 }
-await parentw.reply(conn.user.jid, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭*Para volver a vincular un sub Bot use su token*`, m, rcanal)
+await parentw.reply(conn.user.jid, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Para volver a vincular un sub Bot use su token*`, m, rcanal)
 }
 }
 const timeoutId = setTimeout(() => {
