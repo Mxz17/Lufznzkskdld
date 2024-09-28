4169f8a9290bde90041694b855fe43d4ac5e0ec2
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-28 01:13:29 -0300

    Actualizar jadibot-botones.js

--- plugins/jadibot-botones.js
+++ plugins/jadibot-botones.js
@@ -2,7 +2,7 @@ let media = 'https://iili.io/dsS9lja.md.png'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 await m.react('☕');
-let str = `ʙʏ: ᴀɴɢᴇʟ-ᴏꜰᴄ ʏ ɢᴇɴᴇꜱɪꜱ`
+let str = `ʙʏ ʟᴜғғʏ ᴀɪ`
 await conn.sendButton(m.chat, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ 𝗖𝗢𝗠𝗢 𝗗𝗘𝗦𝗘𝗔 𝗩𝗜𝗡𝗖𝗨𝗟𝗔𝗥\n\n• 𝗦𝗘𝗟𝗘𝗖𝗖𝗜𝗢𝗡𝗔 𝗨𝗡 𝗕𝗢𝗧𝗢𝗡.\n`, str, media, [
 ['🍿Qʀ', '.serbotqr'],
 ['🍿̸̷̸̷᮫᮫໋໋݂݂ׄׄ.ᴄᴏᴅɪɢᴏ', '.serbotcode']], null, [
