e83d8c7ad5d5b4b2989b1f9dea6be15cbe6aeba4
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-28 03:12:39 -0300

    Actualizar gc-demote.js

--- plugins/gc-demote.js
+++ plugins/gc-demote.js
@@ -8,8 +8,8 @@ var number = text.split`@`[1]
 var number = text
 }
 
-if (!text && !m.quoted) return conn.reply(m.chat, `🚩 *Mensione a un administrador para usar este comando.*`, m, rcanal)
-if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `✨️ *Error, debe de mensionar a un administrador.*`, m, fake)
+if (!text && !m.quoted) return conn.reply(m.chat, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Mensione a un administrador para usar este comando.*`, m, rcanal)
+if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Error, debe de mensionar a un administrador.*`, m, fake)
 
 try {
 if (text) {
