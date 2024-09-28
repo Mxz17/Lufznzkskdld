9914391d8f9125782124b7f799d86cdded91b30e
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-28 00:58:59 -0300

    Actualizar mods-autoadmin.js

--- plugins/mods-autoadmin.js
+++ plugins/mods-autoadmin.js
@@ -1,18 +1,18 @@
 const handler = async (m, {conn, isAdmin, groupMetadata }) => {
-  if (isAdmin) return m.reply('> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *¡YA ERES ADM JEFE!*',m, rcanal);
+  if (isAdmin) return m.reply('> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *¡YA ERES ADM AMOR!*',m, rcanal);
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
   await m.react(done)
-   m.reply('> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *¡YA TE DI ADM MI JEFE!*', m, rcanal);
+   m.reply('> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *¡YA TE DI ADM MI VEVE!*', m, rcanal);
     let nn = conn.getName(m.sender);
-     conn.reply('5351524614@s.whatsapp.net', `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, rcanal, );
+     conn.reply('5351524614@s.whatsapp.net', `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, rcanal, );
   } catch {
-    m.reply('> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ Ocurrio un error.');
+    m.reply('> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ Ocurrio un error.');
   }
 };
 handler.tags = ['mods'];
 handler.help = ['autoadmin'];
-handler.command = ['autoadmin'];
+handler.command = ['admin'];
 handler.rowner = true;
 handler.group = true;
 handler.botAdmin = true;
