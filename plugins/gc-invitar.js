179a8530f491cb0465d17edeca0f73ced6dc0ab0
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-28 03:00:01 -0300

    Actualizar gc-invitar.js

--- plugins/gc-invitar.js
+++ plugins/gc-invitar.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-if (!text) throw `🤍 Ingrese el número al que quiere enviar una invitación al grupo\n\n🤍 Ejemplo :\n*${usedPrefix + command}* 573218138672`
-if (text.includes('+')) throw  `🤍 Ingrese el número todo junto sin el *+*`
-if (isNaN(text)) throw '🤍 Ingrese sólo números más su código de país sin espacios'
+if (!text) throw `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Ingrese el número al que quiere enviar una invitación al grupo*\n\n> ⌌᭨⌍ *Ejemplo :*\n*${usedPrefix + command}* 573218138672`
+if (text.includes('+')) throw  `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Ingrese el número todo junto sin el *+*᭄*`
+if (isNaN(text)) throw '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Ingrese sólo números más su código de país sin͟ espacios*'
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
 await conn.reply(text+'@s.whatsapp.net', `🤍 *INVITACIÓN A GRUPO*\n\nUn usuario te invitó a unirte a este grupo \n\n${link}`, m, {mentions: [m.sender]})
-m.reply(`🤍 Se envió un enlace de invitación al usuario.`) 
+m.reply(`> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Se envió un enlace de invitación al usuario.*`) 
 
 }
 handler.help = ['invite *<numero>*']
