791bb61577f2d880a168ce8864f24323908a1457
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-27 23:21:03 -0300

    Actualizar rpg-robarcorazones.js

--- plugins/rpg-robarcorazones.js
+++ plugins/rpg-robarcorazones.js
@@ -2,18 +2,18 @@ const ro = 30;
 const handler = async (m, {conn, usedPrefix, command}) => {
   const time = global.db.data.users[m.sender].lastrob2 + 7200000;
   if (new Date - global.db.data.users[m.sender].lastrob2 < 7200000) {
-  conn.reply(m.chat, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *Hey! esperá ${msToTime(time - new Date())} para volver a robar*`, m, rcanal);
+  conn.reply(m.chat, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Hey! esperá ${msToTime(time - new Date())} para volver a robar*`, m, rcanal);
   return;
   }
   let who;
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
   else who = m.chat;
   if (!who) {
-  conn.reply(m.chat, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *Etiqueta a alguien para robar.*`, m, rcanal)
+  conn.reply(m.chat, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Etiqueta a alguien para robar.*`, m, rcanal)
   return;
     };
   if (!(who in global.db.data.users)) { 
-  conn.reply(m.chat, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *El  pendejo no se encuentra en mi Base de datos 😕.*`, m, rcanal)
+  conn.reply(m.chat, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *El  pendejo no se encuentra en mi Base de datos 😕.*`, m, rcanal)
 return;
   }
   const users = global.db.data.users[who];
@@ -21,7 +21,7 @@ return;
   if (users.corazones < rob) return conn.reply(m.chat, `😔 @${who.split`@`[0]} tiene menos de *${ro} Corazones*\n> No robes a un hambriento 🥺`, m, {mentions: [who]});
   global.db.data.users[m.sender].corazones += rob;
   global.db.data.users[who].corazones -= rob;
-  conn.reply(m.chat, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *Robaste ${rob} Corazones 🤍 a @${who.split`@`[0]}*`, m, {mentions: [who]});
+  conn.reply(m.chat, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Robaste ${rob} Corazones 🍿 a @${who.split`@`[0]}*`, m, {mentions: [who]});
   global.db.data.users[m.sender].lastrob2 = new Date * 1;
 };
 handler.help = ['rob2'];
