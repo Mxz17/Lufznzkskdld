ca225bae25c106f8f04e5e5cf8c37a143dabf45c
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-28 02:40:27 -0300

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -2,7 +2,7 @@ var handler = async (m, { conn, args }) => {
 
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.reply(m.chat, '> 𖤃·۪۫ˑ🍿ֳ᷌᷈͜╳⃟\v' + link, m, rcanal, { detectLink: true })
+conn.reply(m.chat, '> ˑீ͜🍿ֳ᷌᷈͜╳⃟\v' + link, m, rcanal, { detectLink: true })
 
 }
 handler.help = ['link']
