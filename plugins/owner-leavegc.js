723b09065e43d6e57dda4844b7961e76d774f93e
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-28 00:32:58 -0300

    Actualizar owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
 let chat = global.db.data.chats[m.chat]
 chat.welcome = false
-await conn.reply(id, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *GenesisBot* Abandona El Grupo, Fué Genial Estar Aquí 👋᭄`) 
+await conn.reply(id, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Luffy ᵃⁱ* Abandona El Grupo, Fué Genial Estar Aquí 👋᭄`) 
 await conn.groupLeave(id)
 try {  
 chat.welcome = true
