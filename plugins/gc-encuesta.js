01a9c49a80d9ae2b36884b3b90b2d28a7c5b65d3
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-28 03:12:02 -0300

    Actualizar gc-encuesta.js

--- plugins/gc-encuesta.js
+++ plugins/gc-encuesta.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
-if (!args[0]) throw `⚠️️ *_Ingrese un texto para iniciar la escuesta._*\n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
-if (!text.includes('|')) throw  `⚠️️ Separe las encuestas con *|* \n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
+if (!args[0]) throw `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Ingrese un texto para iniciar la escuesta.*\n\n> ◬ *Ejemplo :* \n*${usedPrefix + command}* texto|texto2...`
+if (!text.includes('|')) throw  `> ۣۜ༓ Separe las encuestas con *|* \n\n📌᭄ *E᪶۫۫je᪶۫۫m᪶۫۫pl᪶۫۫o :* \n*${usedPrefix + command}* texto|texto2...`
 let a = []
 let b = text.split('|')
 for (let c = 0; c < b.length; c++) {
