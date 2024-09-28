dc1d683831cebaf288a075d2259fa52d4bfe1249
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-09-28 00:44:22 -0300

    Actualizar owner-blocklist.js

--- plugins/owner-blocklist.js
+++ plugins/owner-blocklist.js
@@ -2,7 +2,7 @@
 
 const handler = async (m, {conn}) => {
   await conn.fetchBlocklist().then(async (data) => {
-    let txt = `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ *≡ Lista de bloqueados*\n\n*Total :* ${data.length}\n\n┌─⊷\n`;
+    let txt = `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *≡ Lista de bloqueados*\n\n*Total :* ${data.length}\n\n┌─⊷\n`;
     for (const i of data) {
       txt += `▢ @${i.split('@')[0]}\n`;
     }
@@ -10,7 +10,7 @@ const handler = async (m, {conn}) => {
     return conn.reply(m.chat, txt, m, {mentions: await conn.parseMention(txt)});
   }).catch((err) => {
     console.log(err);
-    throw '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🤍᪶۫۫྆•≭ No hay números bloqueados';
+    throw '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ No hay números bloqueados';
   });
 };
 handler.help = ['blocklist'];
