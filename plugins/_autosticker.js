15e9023e4f3f4cb3c3f3a31bcffcc6edbec650e7
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-10-02 20:28:12 -0300

    Actualizar _autosticker.js

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -16,7 +16,7 @@ let img = await q.download?.()
 if (!img) return
 stiker = await sticker(img, false, packname, author)
 } else if (/video/g.test(mime)) {
-if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return await m.reply('᥀·࣭࣪̇˖🤍  *El video no debe de durar mas de 7 segundos, intentalo de nuevo.*')
+if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return await m.reply('> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭  *El video no debe de durar mas de 7 segundos, intentalo de nuevo.*')
 
 let img = await q.download()
 if (!img) return
