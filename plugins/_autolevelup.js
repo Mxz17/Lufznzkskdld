8d27174d0153324559abb742a50de8574ed353f5
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-10-02 11:26:02 -0300

    Actualizar _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -11,6 +11,6 @@ let before = user.level * 1
 while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
  if (before !== user.level) {
 
-conn.reply(m.chat, `🤍 *SUBISTE DE NIVEL* 🤍\n\n☁️ *Nuevo Nivel:* ${user.level}\n☁️ *Nivel Anterior:* ${before}\n☁️ *Rango:* ${user.role}\n☁️ *Fecha:* ${new Date().toLocaleString('id-ID')}`.trim(),m, rcanal, )
+conn.reply(m.chat, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ 𓏲୭᳟ *SUBISTE DE NIVEL 𓏲᭔᷼⁩  \n\n▾˚▵˙▸⁩ *Nuevo Nivel:* ${user.level}\n🜲⁩ *Nivel Anterior:* ${before}\n ✿ᩧ *Rango:* ${user.role}\n⌘ *Fecha:* ${new Date().toLocaleString('id-ID')}`.trim(),m, rcanal, )
     }
 }
