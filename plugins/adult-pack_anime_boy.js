ea002c35f3e9bb9cfcb99b370592a5c546b5afa5
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-10-02 20:50:28 -0300

    Actualizar adult-pack_anime_boy.js

--- plugins/adult-pack_anime_boy.js
+++ plugins/adult-pack_anime_boy.js
@@ -4,7 +4,7 @@ let handler = async (m, {command, conn, usedPrefix}) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/Incongnito45/ExoticoBot-MD/master/NODE_EXOTICO_JSON/node-exotico-anime-boy/hot-${command}.json`)).data  
 let haha = await res[Math.floor(res.length * Math.random())]  
 if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Los  comandos para adultos están desactivados, Use el comando.on modohorny.*` 
-conn.sendButton(m.chat, `🔥✵ *${command}* ✵🔥`.trim(), packname, haha, [['🍿➭ Siguente ', `${usedPrefix + command}`]], null, null, m)    
+conn.sendButton(m.chat, `🔥✵ *${command}* ✵🔥`.trim(), packname, haha, [['🍿̇̇ू➭ Siguente ', `${usedPrefix + command}`]], null, null, m)    
 }
 handler.command = handler.help = ['takeda', 'asuma', 'endeavor']
 handler.tags = ['nsfw']
