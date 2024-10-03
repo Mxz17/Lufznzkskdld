67639913d23c8cee58901ed1e02b38f8f78003f3
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-10-03 12:21:09 -0300

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
 let txt_owner = "> ❆ᮬ᭄ *`¡Hola! Soy ꪑאƺ, el creador y administrador de este bot. Estoy aquí para ayudarte y asegurarme de que tengas una experiencia óptima. ¿Necesitas ayuda o tienes sugerencias? No dudes en escribirme. Estoy aquí para ti.`*\n\n ᢀ `+56 9 8329 8345` ⸚"
-await conn.sendFile(m.chat, "https://f.uguu.se/yAgPwLTQ.jpg", 'thumbnail.jpg', txt_owner, m, null, rcanal)
+await conn.sendFile(m.chat, "https://qu.ax/Aszl.jpg", 'thumbnail.jpg', txt_owner, m, null, rcanal)
 }
 handler.help = ['owner']
 handler.tags = ['main']
