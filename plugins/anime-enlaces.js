868a1c3512dc6e93613e5830346db0bfbbb0df31
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-10-02 21:44:19 -0300

    Actualizar anime-enlaces.js

--- plugins/anime-enlaces.js
+++ plugins/anime-enlaces.js
@@ -1,21 +1,21 @@
 var handler = async m => 
 
 conn.reply(m.chat, ` 
-┏━━━━━━━━━━━━━━━━━━━━┅
-┇        ∘̥⃟λиɨრ૯ᮬ ᭨͜❀-”
-┣━━━━━━━━━━━━━━━━━━━━┅
-┃ ꕸ▸ https://kusonime.com
-┃ ꕸ▸ https://huntersekaisub.blogspot.com
-┃ ꕸ▹ https://riie.jp
-┃ ꕸ▹ https://m.meownime.ai
-┃ ꕸ▹ https://meownime.ltd
-┃ ꕸ▹ https://nimegami.id
-┃ ꕸ▹ https://animekompi.cam
-┃ ꕸ▹ https://nontonanimeid.top
-╎╎ ꕸ▸ https://kazefuri.vip
-╎╎ ꕸ▸ https://pendekarsubs.us
-╎╎ ꕸ▸ https://myanimelist.net
-┗━━━━━━━━━━━━━━━━━━━━┅
+╥━━━━━━━━━✧͢⃟ᤢ━━━━━━━━━━
+╎               ∘̥⃟λиɨრ૯ᮬ ᭨͜❀-”
+┞━━━━━━━━━✧͢⃟ᤢ━━━━━━━━━━
+╎ ꕸ▸ https://kusonime.com
+╎ ꕸ▸ https://huntersekaisub.blogspot.com
+╎ ꕸ▹ https://riie.jp
+╎ ꕸ▹ https://m.meownime.ai
+╎ ꕸ▹ https://meownime.ltd
+╎ ꕸ▹ https://nimegami.id
+╎ ꕸ▹ https://animekompi.cam
+╎ ꕸ▹ https://nontonanimeid.top
+╎ ꕸ▸ https://kazefuri.vip
+╎ ꕸ▸ https://pendekarsubs.us
+╎ ꕸ▸ https://myanimelist.net
+╘━━━━━━━━━━━━━━━━━━━━፝⃟༘ஓ
  `, m, rcanal, )
 
 handler.help = ['animelink']
