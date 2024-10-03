3536ceabff2efce14e88a06d9ca0501b18e808f8
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-10-03 13:28:38 -0300

    Actualizar anime-lolice.js

--- plugins/anime-lolice.js
+++ plugins/anime-lolice.js
@@ -3,7 +3,7 @@ var handler = async (m, { conn, usedprefix }) => {
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/lolice', {
 avatar: await conn.profilePictureUrl(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-}), '', '🍿 *L O L I C E* 🌹ᮬ᭄', m)
+}), '', '🍿ુ _\`ᴀ᪶۫۫\` \`ɴ᪶۫۫\` \`ɪ᪶۫۫\` \`ᴍ᪶۫۫\` \`ᴇ᪶۫۫\`_ ૂ🌹ᮬ᭄', m)
 }
 
 handler.help = ['lolice']
