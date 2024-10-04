30670c8be84c8eabb9ae5832e4f4b7dca11eacc7
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-10-04 11:03:16 -0300

    Actualizar buscador-google.js

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -4,8 +4,8 @@ import axios from 'axios'
 let handler = async (m, { conn, command, args, usedPrefix }) => {
 const fetch = (await import('node-fetch')).default;
 const text = args.join` `
-if (!text) return conn.reply(m.chat, '🍟 Ingresa lo que deseas buscar en Google.', m, rcanal)
-conn.reply(m.chat, `🚩 Buscando Su Información...`, m, {
+if (!text) return conn.reply(m.chat, '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Ingresa lo que deseas buscar en Google.*', m, rcanal)
+conn.reply(m.chat, `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Buscando Su Información...*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: wm,
@@ -13,7 +13,7 @@ previewType: 0, thumbnail: icons,
 sourceUrl: canal }}})
 const url = 'https://google.com/search?q=' + encodeURIComponent(text)
 google({'query': text}).then(res => {
-let teks = `🍟 *Resultado de* : ${text}\n\n`
+let teks = `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Resultado de* : ${text}\n\n`
 for (let g of res) {
 teks += `🐢 *Titulo ∙* ${g.title}\n🚩 *Info ∙* ${g.snippet}\n🔗 *Url ∙* ${g.link}\n\n`
 }
