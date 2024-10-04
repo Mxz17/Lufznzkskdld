f3097ae8bb994ed3af3f04e826c1c623a105404b
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-10-04 02:25:07 -0300

    Actualizar prueba-xd.js

--- plugins/prueba-xd.js
+++ plugins/prueba-xd.js
@@ -3,7 +3,7 @@ import { fileTypeFromBuffer } from 'file-type';
 const handler = async (m, {
     args
 }) => {
-    if (!args[0]) return m.reply("> ͓ ♘ ⃟⇱͓͓ *Ejemplo :*\n!notoemoji¡ ⟮ emoji ⟯\n\nUso:\n!notoemoji¡ 🍿")
+  if (!args[0]) return conn.reply(m.chat"> ͓ ♘ ⃟⇱͓͓*Ejemplo :*\n!notoemoji¡ ⟮ emoji ⟯\n\nUso:\n!notoemoji, m, rcanal)
     try {
         m.reply(wait)
         const unicode = await emojiUnicode(args[0])
