029e4c9236d0ccfa83dee62b43b2be48863465a8
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-10-04 18:47:28 -0300

    Actualizar dl-apk.js

--- plugins/dl-apk.js
+++ plugins/dl-apk.js
@@ -22,13 +22,13 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
     const searchResults = await searchApk(text);
     const apkData = await downloadApk(searchResults[0].id);
     const response = `${packname}
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃💫 𝙉𝙤𝙢𝙗𝙧𝙚: ${apkData.name}
-┃📦 𝙋𝘼𝘾𝙆𝘼𝙂𝙀: ${apkData.package}
-┃🕒 𝙐𝙡𝙩𝙞𝙢𝙖 𝘼𝙘𝙩𝙪𝙖𝙡𝙞𝙯𝙖𝙘𝙞𝙤́𝙣: ${apkData.lastup}
-┃💪 𝙏𝙖𝙣𝙖𝙣̃𝙤: ${apkData.size}
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙣𝙙𝙤 𝘼𝙋𝙆 🚀🚀🚀`;
+┣⸻⸻⸻ ⃟ ⃟▫⸻⸻⸻⌔☐❜｡۪۪۫۫
+┋⌦ *Nombre :* ${apkData.name}
+┋⌦ *Packge :* ${apkData.package}
+┋⌦ *Última actl :* ${apkData.lastup}
+┋⌦ *Tamaño :* ${apkData.size}
+┣⸻⸻⸻⸻⸻⸻⌔☐❜｡۪۪۫۫
+┋ *Descargando Apk* 🚀🚀🚀`;
     await conn.sendMessage(m.chat, { image: { url: apkData.icon }, caption: response }, { quoted: m });
     if (apkData.size.includes('GB') || apkData.size.replace(' MB', '') > 999) {
       return await conn.sendMessage(m.chat, { text: '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *EL APK ES MUY PESADO.*',  }, { quoted: m });
