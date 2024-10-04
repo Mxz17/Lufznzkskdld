911dc3bc4fd9acaa02c8b92df595516943968d66
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-10-04 11:29:26 -0300

    Actualizar cmd-list.js

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn}) => {
   conn.reply(m.chat, `
 *< ࿘ Lista de comando  / Textos asignados />*
 
-${Object.entries(global.db.data.sticker).map(([key, value], index) => `*${index + 1}.-*\n ♁  *Código :* ${value.locked ? `*[𝚋𝚕𝚘𝚚𝚞𝚎𝚊𝚍𝚘]* ${key}` : key}\n࿄  *Comando/Texto* ${value.text}`).join('\n\n')}
+${Object.entries(global.db.data.sticker).map(([key, value], index) => `*${index + 1}.-*\n ♁  *Código :* ${value.locked ? `*〈 𝚋𝚕𝚘𝚚𝚞𝚎𝚊𝚍𝚘 〉* ${key}` : key}\n࿄  *Comando/Texto* ${value.text}`).join('\n\n')}
 `.trim(), null, {mentions: Object.values(global.db.data.sticker).map((x) => x.mentionedJid).reduce((a, b) => [...a, ...b], [])});
 };
 handler.command = ['listcmd', 'cmdlist'];
