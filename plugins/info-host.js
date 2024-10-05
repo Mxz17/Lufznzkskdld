1f4c2ecc7cca4632be9f98490516b14bf34fa6a4
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-10-05 14:14:59 -0300

    Actualizar info-host.js

--- plugins/info-host.js
+++ plugins/info-host.js
@@ -18,14 +18,14 @@ La plataforma más rápida y confiable para alojar tus servidores y potenciar tu
 - 
 l-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓-l
 
-*꒰➥ ¿Quieres recargar coins?*⇇۪͎꒱ ꣠ ༘ : : ⁺˖
+*꒰➥ ¿Quieres recargar coins?*⇇۪͎꒱ ꣠  : ⁺˖
 
   ꦿ⃟ۜ💡 *Compra tus coins* con *PayPal* de forma rápida y segura:  
 👉 〖 https://dash.tk-joanhost.com/store 〗
 
 l-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓-l
 
-*꒰➥ 🌍 *¿No tienes PayPal?* :⇇۪͎꒱ ꣠ ༘ : : ⁺˖`࿔*
+*꒰➥ 🌍 *¿No tienes PayPal?* :⇇۪͎꒱ ꣠  : : ⁺˖`
 . 　 · ✦ ¡No te preocupes! 🌍 También aceptamos *pagos internacionales*. Solo envía el monto solicitado en la tienda y el comprobante de pago. Activaremos tus *coins* rápidamente. 
 
 l-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓-l
@@ -36,7 +36,7 @@ l-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓-l
 - 
 l-░░░░░░░░░░░░░░░░░░-l
 
-*꒰➥ 🌍 Para clientes internacionales :⇇۪͎꒱ ꣠ ༘ : : ⁺˖`
+*꒰➥ 🌍 Para clientes internacionales :⇇۪͎꒱ ꣠ : ⁺˖`
 . 　 · ✦ *Mercado Pago*: *02JOAN*  
 . 　 · ✦ *Astropay*: *02JOAN*
 - 
