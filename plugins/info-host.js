7cbbd70b894fa6e0ca706422eb64cc1e64288cf1
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-10-05 04:02:42 -0300

    Actualizar info-host.js

--- plugins/info-host.js
+++ plugins/info-host.js
@@ -1,5 +1,5 @@
 //código editado x Mxz <3
-//▖▗▘▙▚▛▜▝▞▟▖▗▘▙▚▛▜▝▞▟
+//▝▞▟▘▙▚▛▜▝▞▟▖▗▘▙▚▛▜▝
 let handler = async (m, { conn, command, usedPrefix }) => {
 let txt = `👋 *¡Bienvenido a TK-Host  🚀💻*
 La plataforma más rápida y confiable para alojar tus servidores y potenciar tus proyectos. 🌐
