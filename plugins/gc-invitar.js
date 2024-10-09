edd864c98b47ed66f333a75fef43ceda7bcb3528
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-10-09 20:30:04 -0300

    Actualizar gc-invitar.js

--- plugins/gc-invitar.js
+++ plugins/gc-invitar.js
@@ -4,7 +4,7 @@ if (text.includes('+')) throw  `> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆
 if (isNaN(text)) throw '> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Ingrese sólo números más su código de país sin͟ espacios*'
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-await conn.reply(text+'@s.whatsapp.net', `🤍 *INVITACIÓN A GRUPO*\n\nUn usuario te invitó a unirte a este grupo \n\n${link}`, m, {mentions: [m.sender]})
+await conn.reply(text+'@s.whatsapp.net', ` 🍿 *INVITACIÓN A GRUPO*\n\nUn usuario te invitó a unirte a este grupo \n\n${link}`, m, {mentions: [m.sender]})
 m.reply(`> ∙۬◌⃘࣭ٜ࣪࣪࣪۬፝͜🍿᪶۫۫྆•≭ *Se envió un enlace de invitación al usuario.*`) 
 
 }
