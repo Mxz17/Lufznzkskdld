14f5ea41d5258231c3ca2f28a750585fdfed1145
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <maxzgithub@gmail.com>
2024-10-10 12:23:07 -0300

    Actualizar info-runtime.js

--- plugins/info-runtime.js
+++ plugins/info-runtime.js
@@ -2,7 +2,7 @@ let handler = async (m, { usedPrefix, command }) => {
 let uptime = await process.uptime()
 let runtime = `© 🍿͜ᰰ᷒ᮬ✿  ⃕ᴍxᴢ ꔷ⸼݇ . ׄ ೀ
 
-> ̤֟   ᪶᪶᪶۫۫۫۫۫۫͜͜⏦  𝗧𝗶𝗲𝗺𝗽𝗼 𝗔𝗰𝘁𝗶𝘃𝗮: ${rTime(uptime)}`
+> ̤֟   ᪶᪶᪶۫۫۫۫۫۫͜͜⏦  *𝗧𝗶𝗲𝗺𝗽𝗼 𝗔𝗰𝘁𝗶𝘃o :* ${rTime(uptime)}`
 conn.reply(m.chat, runtime, m, { contextInfo: { externalAdReply: { mediaUrl: false, mediaType: 1, description: false, title: packname, body: '𓂅̸̸̸̶̶⃨࠘̿̈⃜  ¡᭸̶꛱̄͟͟͟ 🄻𝕦f̶f̶y̶ E͟ʟ 🅼︎ᴇᴊᴏR̸!◄̸̤̿👒̸̷̨̼̱̱̬', previewType: 0, thumbnail: icons, sourceUrl: canal}}})
 }
 handler.help = ['runtime']
